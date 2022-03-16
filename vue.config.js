d027e44ad6f9844347b79aba990a3e2625326af4
PY-GZKY <341796767@qq.com>
2022-03-16 23:44:31 +0800

    ...

---
+++ vue.config.js
@@ -0,0 +1,25 @@
+
+module.exports = {
+  outputDir: process.env.outputDir,
+  lintOnSave: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? './'
+    : '/',
+  assetsDir: 'dist',
+  devServer: {
+    port: 8001,
+    open: true, //配置自动启动浏览器
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8000', //对应自己的接口
+        changeOrigin: true,
+        ws: true,
+        pathRewrite: {
+          '^/api': '/'
+        }
+      }
+    }
+  },
+
+};
+
