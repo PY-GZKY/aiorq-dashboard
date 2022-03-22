6628ffc90344637457e83fec55d49daf5be38d1c
PY-GZKY <341796767@qq.com>
2022-03-22 16:22:05 +0800

    ...

--- src/components/common/Sidebar.vue
+++ src/components/common/Sidebar.vue
@@ -66,124 +66,21 @@ export default {
           title: "dashboard",
         },
         {
-          icon: "el-icon-s-promotion",
+          icon: "el-icon-lx-home",
           index: "worker",
           title: "worker",
         },
 
         {
-          icon: "el-icon-lx-cascades",
+          icon: "el-icon-lx-home",
           index: "job",
           title: "job",
         },
         {
-          icon: "el-icon-lx-cascades",
+          icon: "el-icon-lx-home",
           index: "logs",
           title: "logs",
         },
-
-        // {
-        //   icon: "el-icon-lx-copy",
-        //   index: "tabs",
-        //   title: "tab选项卡",
-        // },
-
-        // {
-        //   icon: "el-icon-lx-calendar",
-        //   index: "3",
-        //   title: "表单相关",
-        //   subs: [
-        //     {
-        //       index: "form",
-        //       title: "基本表单",
-        //     },
-        //     {
-        //       index: "3-2",
-        //       title: "三级菜单",
-        //       subs: [
-        //         {
-        //           index: "editor",
-        //           title: "富文本编辑器",
-        //         },
-        //         {
-        //           index: "markdown",
-        //           title: "markdown编辑器",
-        //         },
-        //       ],
-        //     },
-        //     {
-        //       index: "upload",
-        //       title: "文件上传",
-        //     },
-        //   ],
-        // },
-
-        // {
-        //     icon: 'el-icon-lx-emoji',
-        //     index: 'icon',
-        //     title: '自定义图标'
-        // },
-
-        // {
-        //   icon: "el-icon-lx-emoji",
-        //   index: "wordcloud",
-        //   title: "词云图",
-        // },
-
-        // {
-        //   icon: "el-icon-s-data",
-        //   index: "charts",
-        //   title: "任务统计",
-        // },
-
-        // {
-        //   icon: "el-icon-tickets",
-        //   index: "workLog",
-        //   title: "任务日志",
-        // },
-
-        // {
-        //   icon: "el-icon-setting",
-        //   index: "setting",
-        //   title: "设置",
-        // },
-
-        // {
-        //     icon: 'el-icon-rank',
-        //     index: '6',
-        //     title: '拖拽组件',
-        //     subs: [
-        //         {
-        //             index: 'drag',
-        //             title: '拖拽列表'
-        //         },
-        //         {
-        //             index: 'dialog',
-        //             title: '拖拽弹框'
-        //         }
-        //     ]
-        // },
-        // {
-        //   icon: "el-icon-lx-global",
-        //   index: "i18n",
-        //   title: "国际化功能",
-
-        // },
-        // {
-        //   icon: "el-icon-lx-warn",
-        //   index: "7",
-        //   title: "错误处理",
-        //   subs: [
-        //     {
-        //       index: "permission",
-        //       title: "权限测试",
-        //     },
-        //     {
-        //       index: "404",
-        //       title: "404页面",
-        //     },
-        //   ],
-        // }
       ],
     };
   },
